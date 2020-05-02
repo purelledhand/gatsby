650ce3a5f38715788486e64729e0d5a8651b78c1
Damien Senger <damien.senger@castoredc.com>
2020-05-02 15:21:43 +0200

    chore(gatsby): migrate pages reducer to TypeScript

--- packages/gatsby/src/redux/reducers/pages.ts
+++ packages/gatsby/src/redux/reducers/pages.ts
@@ -1,31 +1,44 @@
-const normalize = require(`normalize-path`)
+import normalize from "normalize-path"
+import { ActionsUnion, IGatsbyPage, IGatsbyState } from "../types"
 
-module.exports = (state = new Map(), action) => {
+export const pagesReducer = (
+  state: IGatsbyState["pages"] = new Map<string, IGatsbyPage>(),
+  action: ActionsUnion
+): IGatsbyState["pages"] => {
   switch (action.type) {
     case `DELETE_CACHE`:
       return new Map()
+
     case `CREATE_PAGE`: {
       action.payload.component = normalize(action.payload.component)
-      if (!action.plugin && !action.plugin.name) {
+
+      // throws an error if the page is not created by a plugin
+      if (!action.plugin?.id) {
         console.log(``)
         console.error(JSON.stringify(action, null, 4))
         console.log(``)
+
         throw new Error(
-          `Pages can only be created by plugins. There wasn't a plugin set
-        when creating this page.`
+          `Pages can only be created by plugins. There wasn't a plugin set when creating this page.`
         )
       }
+
       // Link page to its plugin.
       action.payload.pluginCreator___NODE = action.plugin.id
       action.payload.pluginCreatorId = action.plugin.id
 
+      // Add page to the state with the path as key
       state.set(action.payload.path, action.payload)
+
       return state
     }
+
     case `DELETE_PAGE`: {
       state.delete(action.payload.path)
+
       return state
     }
+
     default:
       return state
   }
